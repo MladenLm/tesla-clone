f948087401b235275919fd879cebe6437c76095e
MladenLm <mladenlamesevic@gmail.com>
2022-06-21 11:41:39 +0200

    adding elements to the website

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        "tesla-dark": "#393c41"
+      }
+    },
+  },
+  plugins: [],
+}
